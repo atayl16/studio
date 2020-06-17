d3b638adda05f6b05e8b5be337c48a1545381be9
Alisha <atayl16@yahoo.com>
2020-06-18 00:55:49 +0100

    Moved billing to separate page, styling for artifact show and index, button styling, created company sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :artifacts
   resources :companies
-  #get 'home/index'
+  get 'home/index'
   devise_for :users
-  #root to: "home#index"
-  root 'billing#index', as: :billing
+  root to: "home#index"
+  get 'billing/' => 'billing#index', as: :billing
   get '/card/new' => 'billing#new_card', as: :add_payment_method
   post "/card" => "billing#create_card", as: :create_payment_method
   get '/success' => 'billing#success', as: :success
+  post '/subscription' => 'billing#subscribe', as: :subscribe
 
 end
